144976e46abdb129c9004291ad9ced976a9f39d3
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-10-06 01:38:08 +0200

    Commit d'une ligne de fileController
    Juste un commit pour pouvoir pull derrière et terminer la méthode

--- WebApp/WebApi/Controllers/FileController.cs
+++ WebApp/WebApi/Controllers/FileController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SharedCode;
 using WebApi.Models;
 
 namespace WebApi.Controllers
@@ -17,9 +18,10 @@ namespace WebApi.Controllers
         {
             _context = context;
         }
-        public ActionResult<long> Add()
-        {
-
-        }
+        //public ActionResult<long> Add(string name, string body,User creator,DateTime createdAt)
+        //{
+        //    var u = (from e in _context.Users where e.
+        //    _context.Files.Add(new SharedCode.File { Name = name, Body = body, Creator = creator, CreatedAt = DateTime.UtcNow});
+        //}
     }
 }
