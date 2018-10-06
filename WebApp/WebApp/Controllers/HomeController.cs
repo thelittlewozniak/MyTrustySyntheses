cb0011c57d2951fe8f93e10b6fdf3edba58a5e31
thelittlewozniak <nathan.pire@gmail.com>
2018-10-06 11:15:17 +0200

    WALLAH CA MARCHE

--- WebApp/WebApp/Controllers/HomeController.cs
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -2,35 +2,26 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
+        //Index
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        //About
+        public IActionResult AboutUs()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
+            ViewData["Message"] = "GameJam 2018 KYT";
             return View();
         }
 
@@ -38,6 +29,6 @@ namespace WebApp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        }        
     }
 }
