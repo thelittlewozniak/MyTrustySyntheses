eab48263f4655d635f55390e9a6e91865ca32114
Sylvain DUHANT <sylvain.duhant@condorcet.be>
2018-10-06 11:53:47 +0200

    Ajout FileJson

---
+++ WebApp/SharedCode/FileJson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedCode
+{
+    class FileJson
+    {
+        public string Name { get; set; }
+        public string Body { get; set; }
+        public Lesson Lesson { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public long AccessToken { get; set; }
+        virtual public List<Grade> Grades { get; set; }
+    }
+}
